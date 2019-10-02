8b3a6179ec829c2560ea5d6615897b5262898ac9
Jeff Hopper <jeff.hopper@coactiveapps.com>
2019-10-02 16:28:03 -0400

    Nothing fancy

--- test/loadExtension.test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-var loadExtension = require('../src/api/loadExtension');
-
-describe('loadExtension', function() {
-    it('default property should be a function', function() {
-        assert.equal(typeof loadExtension.default, "function");
-    });
-});
